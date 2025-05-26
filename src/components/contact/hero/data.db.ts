25b321502ee859f1d9de2f5b0f1dc379bf51aec4
legiononene <explore.sahil1@gmail.com>
2025-05-26 17:04:01 +0530

    Updated contact us details

--- src/components/contact/hero/data.db.ts
+++ src/components/contact/hero/data.db.ts
@@ -1,41 +1,42 @@
 type ContactUsDataType = {
-    title: string;
-    description: string;
-    info: {
-        contactInfo: {
-            title: string;
-            info: {
-                title: string;
-                value: string;
-                isLink: boolean;
-                prefix?: string;
-            }[];
-        };
+  title: string;
+  description: string;
+  info: {
+    contactInfo: {
+      title: string;
+      info: {
+        title: string;
+        value: string;
+        isLink: boolean;
+        prefix?: string;
+      }[];
     };
+  };
 };
 
 export const contactUsData = {
-    title: "Contact Us",
-    description: "Gramtek is here to help and we'd love to hear from you. Whether you have a question about our sanitary pad distribution, want to explore partnership opportunities, or simply wish to share feedback, our team is ready to connect and support you every step of the way.",
-    info: {
-        contactInfo: {
-            title: "Get in Touch",
-            info: [
-                {
-                    title: "Phone",
-                    value: "+91 123 456 7890",
-                    prefix: "tel:",
-                },
-                {
-                    title: "Email",
-                    value: "hello@gramtek.in",
-                    prefix: "mailto:",
-
-                }, {
-                    title: "Office Hours",
-                    value: "Monday-Friday, 9 AM-6 PM IST",
-                }
-            ],
-        }
-    }
+  title: "Contact Us",
+  description:
+    "Gramtek is here to help and we'd love to hear from you. Whether you have a question about our sanitary pad distribution, want to explore partnership opportunities, or simply wish to share feedback, our team is ready to connect and support you every step of the way.",
+  info: {
+    contactInfo: {
+      title: "Get in Touch",
+      info: [
+        {
+          title: "Phone",
+          value: "+91 827 555 5331",
+          prefix: "tel:",
+        },
+        {
+          title: "Email",
+          value: "info@gramtek.in",
+          prefix: "mailto:",
+        },
+        {
+          title: "Office Hours",
+          value: "Monday-Friday, 11 AM to 5 PM IST",
+        },
+      ],
+    },
+  },
 };
